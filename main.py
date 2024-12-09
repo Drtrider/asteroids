87b15730691ef2334f1951e04ea26c2b71e586d5
Drtrider <drtrider200@gmail.com>
2024-12-09 06:24:26 -0800

    Better comments

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pygame
 from constants import *
 
 def main():
+    # Init the game and some stuff for it
     pygame.init()
-
     game_clock = pygame.time.Clock()
     dt = 0
 
@@ -12,8 +12,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Set the screen size
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,6 +23,8 @@ def main():
 
         screen.fill("Black")
         pygame.display.flip()
+
+        # Controls FPS of the game's window
         current_clock_time = game_clock.tick(60)
         dt = current_clock_time / 1000
     
